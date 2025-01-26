143e7a3136d56675bc50cd3886de372b4cc0ef22
Mehmet Berkan <mehmetberkangorel@gmail.com>
2025-01-26 21:01:08 +0300

    Initial commit

---
+++ spring-redis-category-cache-example/src/main/java/com/mehmetberkan/springrediscategorycacheexample/SpringRedisCategoryCacheExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.springrediscategorycacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisCategoryCacheExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisCategoryCacheExampleApplication.class, args);
+	}
+
+}
